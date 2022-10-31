0cdb653721039c48af7b7d6e3b075ff72a81907a
lotusksm <lotusksm@gmail.com>
2022-10-31 23:50:15 +0900

    Initial commit

---
+++ src/main/java/com/lotus/book/freelecspringboot2webservice1/FreelecSpringboot2Webservice1Application.java
@@ -0,0 +1,13 @@
+package com.lotus.book.freelecspringboot2webservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2Webservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecSpringboot2Webservice1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lotus/book/freelecspringboot2webservice1/FreelecSpringboot2Webservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotus.book.freelecspringboot2webservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2Webservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
